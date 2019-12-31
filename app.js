6a7e4f1f063daff3737dbc3f740a353d6c8150ad
Shaoqing Tan <bettercallshao@gmail.com>
2019-12-31 02:08:12 -0500

    Use pug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
